06ceb22377dfc2509a88dbf950e09f728c13ffe2
Jordi <jordilop@gmail.com>
2022-10-06 09:20:28 +0200

    Exercici 2

--- ts/index.ts
+++ ts/index.ts
@@ -1,14 +1,15 @@
 const API_URL = 'https://icanhazdadjoke.com/';
 
 function validate() {
+    const HTMLResponse = document.getElementById('joke')!;
+
     fetch(API_URL, {
         headers: {
             'Accept': 'application/json'
         }
     })
         .then(response => response.json())
-        .then(data => console.log(data.joke))
+        .then(data => HTMLResponse.innerHTML = data.joke)
         .catch(error => console.log(error));
 }
-
 
