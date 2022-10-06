f6fd6cbb3ca6da0f5fe88804437a0e24deaee991
Jordi <jordilop@gmail.com>
2022-10-06 12:31:26 +0200

    Exercici 3

--- js/index.js
+++ js/index.js
@@ -1,7 +1,11 @@
 "use strict";
 const API_URL = 'https://icanhazdadjoke.com/';
-function validate() {
+const TEXT_INIT = 'Anem a riure una mica... &#128516';
+document.getElementById('joke').innerHTML = TEXT_INIT;
+const reportAcudits = [];
+function nextJoke() {
     const HTMLResponse = document.getElementById('joke');
+    HTMLResponse.innerHTML == TEXT_INIT ? document.getElementById('scores').style.display = "inline" : false;
     fetch(API_URL, {
         headers: {
             'Accept': 'application/json'
@@ -10,4 +14,22 @@ function validate() {
         .then(response => response.json())
         .then(data => HTMLResponse.innerHTML = data.joke)
         .catch(error => console.log(error));
+}
+function saveScore(score) {
+    const joke = document.getElementById('joke').innerHTML;
+    const indexReport = reportAcudits.findIndex(element => element.joke == joke);
+    if (indexReport == -1) {
+        const date = new Date();
+        const acudit = {
+            joke: joke,
+            score: score,
+            date: date.toISOString()
+        };
+        reportAcudits.push(acudit);
+        console.clear();
+        console.log(reportAcudits);
+    }
+    else {
+        alert(`Error! Ja has valorat aquest acudit`);
+    }
 }
--- ts/index.ts
+++ ts/index.ts
@@ -1,7 +1,18 @@
-const API_URL = 'https://icanhazdadjoke.com/';
+const API_URL: string = 'https://icanhazdadjoke.com/';
+const TEXT_INIT: string = 'Anem a riure una mica... &#128516';
 
-function validate() {
+interface IJoke {
+    joke: string,
+    score: number,
+    date: string
+}
+
+document.getElementById('joke')!.innerHTML = TEXT_INIT;
+const reportAcudits: IJoke[] = [];
+
+function nextJoke() {
     const HTMLResponse = document.getElementById('joke')!;
+    HTMLResponse.innerHTML == TEXT_INIT ? document.getElementById('scores')!.style.display = "inline": false;
 
     fetch(API_URL, {
         headers: {
@@ -11,5 +22,23 @@ function validate() {
         .then(response => response.json())
         .then(data => HTMLResponse.innerHTML = data.joke)
         .catch(error => console.log(error));
+}
+
+function saveScore(score: number) {
+    const joke: string = document.getElementById('joke')!.innerHTML;
+    const indexReport: number = reportAcudits.findIndex(element => element.joke == joke);
+    if(indexReport == -1) {
+        const date: Date = new Date();
+        const acudit: IJoke = {
+            joke: joke,
+            score: score,
+            date: date.toISOString()
+        };
+        reportAcudits.push(acudit);
+        console.clear();
+        console.log(reportAcudits);
+    } else {
+        alert(`Error! Ja has valorat aquest acudit`);
+    }
 }
 
